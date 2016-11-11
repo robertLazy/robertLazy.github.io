a1e92f34cbff50892cb5d5a98ebc239876dc4206
robertLazy <lacyr1@wis.edu.hk>
2016-11-11 15:07:29 +0800

    upload game

---
+++ game/js/index.js
@@ -0,0 +1,82 @@
+//alert("Hello");
+
+//prompt("what is your name?");
+
+//var message ="hello! welcome to coding.";
+//var x = 10;
+// alert(x/2)
+
+//var name=prompt("what is your name?");
+//alert('nice to meet you " + name);
+//var noun=prompt("type a noun");
+//var verb=prompt("type a verb");
+//var adjective=prompt("type a adjective");
+//alert("the "+ adjective+ " "+noun+" had to "+ verb)
+//var password = prompt("enter the password");
+//if (password === "coding")
+//  {window.open(google.com);  }
+//else alert("the password is wrong")
+//var color = prompt("what is your //faveroite color?");
+//if(color ==="black")
+//  {alert("me too!")}
+//else if (color === "blue")
+// {alert("thats also a nice color")}
+//else
+// {alert("thats not a nice color")}
+//var fruits = prompt("what is your faveroite fruit?");
+//switch(fruits) {
+//  case "banana": 
+// break;
+
+//  case"strawberries"
+//  alert
+//   
+function myFunction(game){
+var name = prompt("what are you called?");
+var health = 100;
+var kills = 0;
+alert("Hello " + name + ", you are at full health");
+var fight=prompt("have you come to fight?");
+ switch(fight) {
+  case "yes":
+    var weapon=prompt("will you fight with a gun or a knife?");
+      switch(weapon) {
+case "knife":
+  alert("you win the fight, but you have a lot of wounds. 80% health");
+    alert("you have aquired a weapon");
+    health = 80;          
+          break;
+
+ case "gun": 
+
+  alert("you kill him before he pulls the trigger. No health lost");
+  alert("you keep the gun but you have only 3 bullets");
+break;}
+
+  alert("you leave the village and head to the mountains");
+var help = prompt("there is an old man that needs help... do you go over to assist him?");
+switch(help) {
+case "yes": 
+  alert("IT WAS A TRAP!!!");
+  alert("bantits come out from behind the trees and ambush you");
+var ambush=prompt("there are 4 emeimies do you fight or run");
+ switch(ambush) {
+  case "run": 
+  alert("they chase you but you shoot back at them... you kill 2 of them but you are badly injured ");
+ alert("the third hunts you down and kills you");
+ break;
+     case"run": 
+alert("you charge at them but they wip you out");
+alert("you die");
+ break;}
+ exit();   
+    
+     
+     switch(fight){
+       case "no":
+         break;}
+         switch(help) {
+  case "no": 
+ alert("you live");
+             break;
+                    }}}}
